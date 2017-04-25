d1e5d6652eaefddf4b0c6e99685737598c82c997
pizhanska <pizhanska29@gmail.com>
2017-04-25 23:28:16 +0300

    add models for binding

---
+++ src/main/java/com/epam/mentorship/service/model/PostModel.java
@@ -0,0 +1,55 @@
+package com.epam.mentorship.service.model;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name="post")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class PostModel {
+
+    @XmlElement
+    protected int userId;
+
+    @XmlElement
+    protected int id;
+
+    @XmlElement
+    protected int title;
+
+    @XmlElement
+    protected int body;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTitle() {
+        return title;
+    }
+
+    public void setTitle(int title) {
+        this.title = title;
+    }
+
+    public int getBody() {
+        return body;
+    }
+
+    public void setBody(int body) {
+        this.body = body;
+    }
+}
---
+++ src/main/java/com/epam/mentorship/service/model/UserModel.java
@@ -0,0 +1,7 @@
+package com.epam.mentorship.service.model;
+
+/**
+ * Created by Uliana Pizhanska on 25/04/2017.
+ */
+public class UserModel {
+}
