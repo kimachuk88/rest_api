0664bb757850960c78b5368db73159798223f311
pizhanska <pizhanska29@gmail.com>
2017-04-27 23:20:20 +0300

    create Report Appender

---
+++ src/main/java/com/epam/mentorship/service/utilities/ReportAppender.java
@@ -0,0 +1,45 @@
+package com.epam.mentorship.service.utilities;
+
+/**
+ * Created by Uliana Pizhanska on 27/04/2017.
+ */
+
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.Layout;
+import org.apache.log4j.spi.LoggingEvent;
+import org.testng.Reporter;
+
+public class ReportAppender extends AppenderSkeleton {
+
+    @Override
+    protected void append(final LoggingEvent event)
+    {
+        Reporter.log(eventToString(event));
+    }
+
+    private String eventToString(final LoggingEvent event)
+    {
+        final StringBuilder result = new StringBuilder(layout.format(event));
+
+        if(layout.ignoresThrowable())
+        {
+            final String[] s = event.getThrowableStrRep();
+            if (s != null)
+            {
+                for (final String value : s)
+                {
+                    result.append(value).append(Layout.LINE_SEP);
+                }
+            }
+        }
+        return result.toString() + "</br>";
+    }
+
+    public void close() {
+
+    }
+
+    public boolean requiresLayout() {
+        return true;
+    }
+}
